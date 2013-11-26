d2efe725c9e6047b7399ca04b307dc335b7231d3
Max Fu <fxy83613@gmail.com>
2013-11-26 05:16:07 +0000

    Added Adam boot and PhilZ recovery ramdisks of GT-I9500.

---
+++ ramdisks/GT-I9500/boot-ramdisk/sbin/adam.sh
@@ -0,0 +1,54 @@
+#!/sbin/busybox sh
+# Adam kernel script (Root helper by Wanam)
+
+/sbin/busybox mount -t rootfs -o remount,rw rootfs
+
+#Disable knox
+pm disable com.sec.knox.seandroid
+setenforce 0
+
+chown 0.0 /system/xbin/su
+chmod 06755 /system/xbin/su
+symlink /system/xbin/su /system/bin/su
+
+chown 0.0 /system/xbin/daemonsu
+chmod 06755 /system/xbin/daemonsu
+
+
+chown 0.0 /system/app/Superuser.apk
+chmod 0644 /system/app/Superuser.apk
+
+chown 0.0 /system/app/STweaks.apk
+chmod 0644 /system/app/STweaks.apk
+
+if [ ! -f /system/xbin/busybox ]; then
+ln -s /sbin/busybox /system/xbin/busybox
+ln -s /sbin/busybox /system/xbin/pkill
+fi
+
+if [ ! -f /system/bin/busybox ]; then
+ln -s /sbin/busybox /system/bin/busybox
+ln -s /sbin/busybox /system/bin/pkill
+fi
+
+chmod -R 755 /res/customconfig/actions
+
+rm /data/.adamkernel/customconfig.xml
+rm /data/.adamkernel/action.cache
+
+/system/bin/setprop pm.sleep_mode 1
+/system/bin/setprop ro.ril.disable.power.collapse 0
+/system/bin/setprop ro.telephony.call_ring.delay 1000
+
+sync
+
+/system/xbin/daemonsu --auto-daemon &
+
+if [ -d /system/etc/init.d ]; then
+  /sbin/busybox run-parts /system/etc/init.d
+fi
+
+chmod 755 /res/uci.sh
+/res/uci.sh apply
+
+/sbin/busybox mount -t rootfs -o remount,ro rootfs
---
+++ ramdisks/GT-I9500/recovery-ramdisk/sbin/postrecoveryboot.sh
@@ -0,0 +1,7 @@
+#!/sbin/sh
+
+sleep 2
+echo 0 > /sys/devices/platform/s3c2440-i2c.0/i2c-0/0-0020/input/input1/enabled
+echo on > /sys/devices/platform/s3c2440-i2c.0/i2c-0/0-0020/input/input1/power/control
+echo 1 > /sys/devices/platform/s3c2440-i2c.0/i2c-0/0-0020/input/input1/enabled
+
